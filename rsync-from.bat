19b4203b17b48bdefad042627b29abd3f5e68a38
ffreemt <yucongo+fmt@gmail.com>
2022-12-25 15:53:11 +0800

    Update menuTemplate

--- rsync-from.bat
+++ rsync-from.bat
@@ -1,2 +1,5 @@
 REM C:\apps\cwrsync_6.2.5_x64_free\bin\rsync.exe -uvazn /cygdrive/c/mat-dir/playground/github/ptextpad-electron/ ./ --exclude node_modules --exclude python --exclude .git --exclude dist
-C:\apps\cwrsync_6.2.5_x64_free\bin\rsync.exe -uvaz /cygdrive/c/mat-dir/playground/github/ptextpad-electron/ ./ --exclude node_modules --exclude python --exclude .git --exclude dist
+
+REM C:\apps\cwrsync_6.2.5_x64_free\bin\rsync.exe -uvaz /cygdrive/c/mat-dir/playground/github/ptextpad-electron/ ./ --exclude node_modules --exclude python --exclude .git --exclude dist
+
+C:\apps\cwrsync_6.2.5_x64_free\bin\rsync.exe -uvazn  /cygdrive/c/mat-dir/playground/github/ptextpad-electron/ ./ --exclude node_modules --exclude python --exclude .git --exclude dist --exclude try-stuff --exclude package-lock.json --exclude data
